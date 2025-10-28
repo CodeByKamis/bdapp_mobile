f99682333d409f549bf7c976066edf152c6edc9f
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-10-28 11:28:23 -0300

    parcialmente pronto, falta estilizar e tal

---
+++ bdapp/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+
+void main() async {//asyn porq ele espera o firebase conecar no app
+  WidgetsFlutterBinding.ensureInitialized(); //função para inicializar os componentes que utilizam o firebase
+  await Firebase.initializeApp( //aguarda o firebase iniciar
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TelaGet(),
+    );
+  }
+}
+class TelaGet extends StatefulWidget {
+  const TelaGet({super.key});
+
+  @override
+  State<TelaGet> createState() => _TelaGetState();
+}
+
+class _TelaGetState extends State<TelaGet> {
+  String? temperatura; //a variavel pode ser nula, caso o database não exista
+  @override
+  void initState(){
+    super.initState();
+    gettemp();
+  }
+  void gettemp(){
+    //collection é o nome da sua coleçõ, precisa ser o mesmo nome escolhido no banco de dados
+    FirebaseFirestore.instance.collection("monitoramento").snapshots().listen(//snapsshots é os dados retornados do banco e o listen é para listar eles
+    (snapshot){//o que vc irá fazer para cada um?
+    //dynamic pode ser qualque tipo de dado
+    dynamic data = snapshot.docs.first.data();//data == ao primeiro documento que tem no seu banco
+    setState(() {
+      temperatura = data['temperatura'];
+    });
+
+    }
+
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Text("$temperatura")
+          ],
+        )
+      )
+    );
+  }
+}
